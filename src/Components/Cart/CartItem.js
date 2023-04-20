252789922fdc8fc7abd9e1ae1ff331d491097131
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-04-20 15:17:10 -0400

    rendered name and price in user's cart

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,8 +1,14 @@
+import dataDishes from "../../data/dataDishes";
+
 const CartItem = ({item})=> {
-    console.log(item)
+console.log(item)
+    const dishes = dataDishes.find(dish =>  dish.id === item.dishId)
+
     return(
         <div>
-            <h4>{item.quantity}</h4>
+            <span>{item.quantity} {item.quantity > 1? "portions" : "portion"}</span>
+            <span> of {dishes.name}</span>
+            <span> ${dishes.price * item.quantity}</span>
         </div>
 
 )
