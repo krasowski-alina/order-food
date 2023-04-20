b6b8fcc9bca5dc0f2c53d061866cf03c00817d0a
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-04-20 15:53:54 -0400

    rendered total price for selected items in a cart

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -9,12 +9,17 @@ export const cartSlice = createSlice ({
         addItemToCart: (state, action) => {
             state.cartItems.push({
                 dishId: action.payload.dish.id,
-                quantity: action.payload.quantity
+                quantity: action.payload.quantity,
+                totalPrice: action.payload.quantity * action.payload.dish.price
             })
         }
     }
 })
-
+export const getTotalPrice = state => {
+    return state.cart.cartItems.reduce((total, cartItems)=> {
+        return total + cartItems.totalPrice
+    }, 0)
+}
 export const getCartItems = state => state.cart.cartItems
 export const { addItemToCart } = cartSlice.actions
 export default cartSlice.reducer
